b9b231b47b5238fc978b06a3f209b1349278222c
Ouassim El-Morabit <oel-mora@c2r6p4.1337.ma>
2026-03-26 21:25:05 +0100

    done

--- Python00/ex1/ft_garden_name.py
+++ Python00/ex1/ft_garden_name.py
@@ -1,7 +1,5 @@
-def ft_garden_summary():
+def ft_garden_name():
     name = input("Enter garden name: ")
-    number = input("Enter number of plants: ")
 
     print(f"Garden: {name}")
-    print(f"Plants: {number}")
     print("Status: Growing well!")
